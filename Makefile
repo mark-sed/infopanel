d55ac8e81874bb376889ab0208f69f35f86d8d8d
Marek Sedlacek <mr.mareksedlacek@gmail.com>
2021-05-29 01:11:59 +0200

    Added HSV number generation

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ SRC=$(wildcard libs/rpi_ws281x/*.c)
 FILES=info_panel.cpp led_matrix.cpp
 CC=g++
 FLAGS=-Wall -fpermissive -std=c++17 -g
+LIBS=-lm
 
 build: $(SRC)
-	$(CC) $(FLAGS) -o info_panel $(FILES) $^
+	$(CC) $(FLAGS) -o info_panel $(FILES) $^ $(LIBS)
--- led_matrix.cpp
+++ led_matrix.cpp
@@ -1,5 +1,8 @@
 #include "led_matrix.hpp"
 #include <stdint.h>
+#include <stdlib.h>
+#include <ctime>
+#include <cmath>
 
 #include <iostream>
 
@@ -35,10 +38,44 @@ LEDMatrix::~LEDMatrix(){
     ws2811_fini(&this->ledstring);
 }
 
+static uint32_t hsv2rgb(float H, float S, float V){
+    float s = S/100;
+    float v = V/100;
+    float C = s*v;
+    float X = C*(1-abs(fmod(H/60.0, 2)-1));
+    float m = v-C;
+    float r,g,b;
+    if(H >= 0 && H < 60){
+        r = C,g = X,b = 0;
+    }
+    else if(H >= 60 && H < 120){
+        r = X,g = C,b = 0;
+    }
+    else if(H >= 120 && H < 180){
+        r = 0,g = C,b = X;
+    }
+    else if(H >= 180 && H < 240){
+        r = 0,g = X,b = C;
+    }
+    else if(H >= 240 && H < 300){
+        r = X,g = 0,b = C;
+    }
+    else{
+        r = C,g = 0,b = X;
+    }
+    uint32_t R = (r+m)*255;
+    uint32_t G = (g+m)*255;
+    uint32_t B = (b+m)*255;
+    return ((R<<16) + (G<<8) + B);
+}
+
 void LEDMatrix::test(){
+    const float GOLDEN_RATION = 0.618033988749895;
+    std::srand(std::time(nullptr));
     for(int i = 1; i < width*height-1; i++){
-	this->pixels[i-1] = 0;
-	std::cout << std::hex << this->pixels[i-1] << ", ";
+        float h = std::rand() + GOLDEN_RATION;
+        h = std::fmod(h, 1);
+        this->pixels[i-1] = hsv2rgb(h, 0.99, 0.99);
     }
 
     this->render();
