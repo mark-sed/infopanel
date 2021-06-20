d8aab9473c355959cad28d0b456d29afedde27ea
Marek Sedlacek <mr.mareksedlacek@gmail.com>
2021-06-21 00:42:24 +0200

    Added missing render

--- info_panel.cpp
+++ info_panel.cpp
@@ -21,6 +21,7 @@ bool is_market_open(){
 void wall_clock() {
     FontAscii ascii;
     auto sc = SimpleClock(std::move(ascii));
+    sc.draw(matrix);
     int pos = -16+matrix.get_text_width()/2;
     while(true){
         sc.draw(matrix);
