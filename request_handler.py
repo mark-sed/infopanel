b8a2849f072ce25e11fa7b37a1d4c44ddb79deb1
Marek Sedlacek <mr.mareksedlacek@gmail.com>
2021-12-03 21:27:00 +0100

    Adde dummy server settings (using -d or --dummy option)

--- request_handler.py
+++ request_handler.py
@@ -1,66 +1,155 @@
 import json
 import stockpanel
-import threading
+from server.swagger_server.models.scheduler import Scheduler 
+from sys import argv
 
+def is_dummy():
+    return len(argv) > 1 and (argv[1] == "-d" or argv[1] == "--dummy")
 
 def hello():
+    if is_dummy():
+        return "Pong"
     return stockpanel.hello()
 
 def setBrightness(value):
+    if is_dummy():
+        return True
     return stockpanel.setBrightness(value)
 
 def showStocks():
+    if is_dummy():
+        return True
     return stockpanel.showStocks()
 
 def toggleLamp():
+    if is_dummy():
+        return True
     return stockpanel.toggleLamp()
 
 def toggleDisplay():
+    if is_dummy():
+        return True
     return stockpanel.toggleDisplay()
 
 def showClock(time_sec):
+    if is_dummy():
+        return True
     return stockpanel.showClock(time_sec if time_sec is not None else 10)
     
 def setCurrency(name):
+    if is_dummy():
+        return True
     return stockpanel.setCurrency(name)
 
 def setFinhubKey(key):
+    if is_dummy():
+        return True
     return stockpanel.setFinhubKey(key)
 
 def setPolygonKey(key):
+    if is_dummy():
+        return True
     return stockpanel.setPolygonKey(key)
 
 def setScheduler(jsonStr):
+    if is_dummy():
+        return True
     return stockpanel.setScheduler(jsonStr)
 
 def setColorSymbol(code):
+    if is_dummy():
+        return True
     return stockpanel.setColorSymbol(code)
 
 def setColorPrice(code):
+    if is_dummy():
+        return True
     return stockpanel.setColorPrice(code)
 
 def setColorGain(code):
+    if is_dummy():
+        return True
     return stockpanel.setColorGain(code)
 
 def setColorLoss(code):
+    if is_dummy():
+        return True
     return stockpanel.setColorLoss(code)
 
 def setColorNeutral(code):
+    if is_dummy():
+        return True
     return stockpanel.setColorNeutral(code)
 
 def getSettings():
+    if is_dummy():
+        return json.dumps(
+            {
+            "brightness": 42,
+            "currency": "usd",
+            "finhubKey": "1242DEADBEEF456",
+            "polygonKey": "314159265ABC",
+            "colors": {
+                "symbol": "0xFCBA03",
+                "price": "0xFCBA03",
+                "gain": "0x10AA10",
+                "loss": "0xAA1010",
+                "neutral": "0xFCBA03"
+                }
+            }
+        )
     return stockpanel.getSettings()
 
 def getBrightness():
+    if is_dummy():
+        return json.dumps({"value": 42})
     return stockpanel.getBrightness()
 
 def getScheduler():
+    if is_dummy():
+        return json.dumps(
+        {
+        "widgets": {
+            "stocks": [
+            {
+                "position": 0,
+                "tickers": [
+                    "NIO", "F", "INTC"
+                ],
+                "showPrice": True,
+                "showPercentage": True
+            }
+            ],
+            "crypto": [
+            {
+                "position": 1,
+                "tickers": [
+                    "bitcoin", "ethereum"
+                ],
+                "showPrice": True,
+                "showPercentage": True
+            }
+            ],
+            "clock": [
+            {
+                "position": 2,
+                "format24hr": True,
+                "font": "Ascii",
+                "type": "Pixelated"
+            }
+            ]
+        }
+        })
     return stockpanel.getScheduler()
 
 def getDisplayStatus():
+    if is_dummy():
+        return json.dumps({"isOn": True})
     return stockpanel.getDisplayStatus()
 
 def getLampStatus():
+    if is_dummy():
+        return json.dumps({"isOn": True})
     return stockpanel.getLampStatus()
 
 def initPanel():
--- server/swagger_server/__main__.py
+++ server/swagger_server/__main__.py
@@ -8,25 +8,22 @@ from sys import path
 path.append("../")
 import request_handler
 
-def r():
-    print("AAA")
-    app.run(port=8080)
 
 def main():
-    request_handler.initPanel()
-    #th_panel = threading.Thread(target=request_handler.runPanel)
-    #th_panel.setDaemon(True)
+    dummy = request_handler.is_dummy()
+    if dummy:
+        print("WARNING: Running in DUMMY mode.")	
+    if not dummy:
+    	request_handler.initPanel()
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Stock Panel'})
-    #app.run(port=8080)
     th_server = threading.Thread(target=app.run, args=(8080,))
     th_server.setDaemon(True)
     th_server.start()
-    while True:
+    while not dummy:
         request_handler.runPanel()
     th_server.join()
-    #th_panel.join()
 
 
 if __name__ == '__main__':
